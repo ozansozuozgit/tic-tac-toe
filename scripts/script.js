192e704b213006b11afdba43b266e7a3d876a209
Ozan Sozuoz <ozansozuoz@hotmail.com>
2020-04-29 17:12:39 +0300

    added replay functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,37 @@
 const Player = (name, id, active = false) => {
   const getName = () => name;
   const getId = () => id;
-  let ative = active;
 
   return { getName, getId, active };
 };
 
-const Player1 = Player("Ozan", 1, true);
-const Player2 = Player("John", 2);
-
-const playerArray = [Player1, Player2];
-
 const gameBoard = (() => {
-  const gameArray = [...Array(3)].map(() => Array(3).fill(0)); // 3 rows, 3 collumns
+  let playerArray = [];
+  const getPlayers = () => playerArray;
+  const setPlayers = () => {
+    playerArray = [];
+    const [player1Name, player2Name] = document.querySelectorAll("input");
+    const Player1 = Player(player1Name.value, 1, true);
+    const Player2 = Player(player2Name.value, 2);
+    playerArray.push(Player1, Player2);
+  };
+
+  let gameArray = [];
+  // 3 rows, 3 columns
+  const createArray = () => (gameArray = [...Array(3)].map(() => Array(3).fill(0)));
   const setArray = (row, col, value) => {
     if (!(gameArray[row][col] === 0)) return; // stop if cell is full
     gameArray[row][col] = value;
-    console.table(gameArray);
   };
+  const getArray = () => gameArray;
 
-  return { gameArray, setArray };
+  return { gameArray, setArray, playerArray, setPlayers, getPlayers, getArray, createArray };
 })();
 
 const displayController = (() => {
-  // display winner as well
+  const container = document.querySelector("#container");
 
-  const displayBoard = (gameArray) => {
-    const container = document.querySelector("#container");
+  const displayBoard = () => {
     container.style.display = "grid";
     for (let i = 0; i < 3; i++) {
       for (k = 0; k < 3; k++) {
@@ -38,42 +43,65 @@ const displayController = (() => {
     }
   };
 
+  const hideBoard = () => {
+    container.style.display = "none";
+  };
+
   const displaySelection = (cell) => {
     if (cell.hasChildNodes()) return;
     const img = document.createElement("img");
     // console.log(Player1.getActive());
-    img.src = Player1.active === true ? "images/X.png" : "images/O.jpeg";
+    img.src = gameBoard.getPlayers()[0].active === true ? "images/X.png" : "images/O.jpeg";
     cell.appendChild(img);
   };
-  return { displaySelection, displayBoard };
+
+  const displayWinner = (player) => {
+    if (player === "draw") {
+      console.log("It's a draw!");
+    } else {
+      console.log(`${player} wins!`);
+    }
+    updateGame.gameEnd = true;
+    document.querySelector("#replay").style.display = "inline-block";
+  };
+
+  return { displaySelection, displayBoard, displayWinner, hideBoard };
 })();
 
 const updateGame = (() => {
-  const updateArray = (cell, gameBoard) => {
+  const gameEnd = false;
+
+  const updateArray = (cell) => {
     const [row, col] = [...cell.attributes[1].value];
-    const value = Player1.active === true ? "X" : "O";
+    const value = gameBoard.getPlayers()[0].active === true ? "X" : "O";
     gameBoard.setArray(row, col, value);
     console.log(`row:${row} col:${col}`);
   };
 
   // Update Players
   const switchPlayers = () => {
-    for (let player of playerArray) {
+    for (let player of gameBoard.getPlayers()) {
       player.active = !player.active;
     }
   };
 
+  return { updateArray, switchPlayers, gameEnd };
+})();
+
+const gameStatus = (() => {
   const checkWin = (gameArray) => {
+    const currentPlayer =
+      gameBoard.getPlayers()[0].active === true
+        ? gameBoard.getPlayers()[0].getName()
+        : gameBoard.getPlayers()[1].getName();
     // Horizontal check
     for (let i = 0; i < 3; i++) {
       if (
         gameArray[i][0] !== 0 &&
         gameArray[i][0] === gameArray[i][1] &&
         gameArray[i][1] === gameArray[i][2]
-      ) {
-        console.log(`Winner is ${Player1.active === true ? Player1.getName() : Player2.getName()}`);
-        // Return
-      }
+      )
+        displayController.displayWinner(currentPlayer);
     }
     // Vertical
     for (let i = 0; i < 3; i++) {
@@ -81,40 +109,55 @@ const updateGame = (() => {
         gameArray[0][i] !== 0 &&
         gameArray[0][i] === gameArray[1][i] &&
         gameArray[1][i] === gameArray[2][i]
-      ) {
-        console.log(`Winner is ${Player1.active === true ? Player1.getName() : Player2.getName()}`);
-      }
+      )
+        displayController.displayWinner(currentPlayer);
     }
     // Diagonal
     if (
       gameArray[0][0] !== 0 &&
       gameArray[0][0] === gameArray[1][1] &&
       gameArray[1][1] === gameArray[2][2]
-    ) {
-      console.log(`Winner is ${Player1.active === true ? Player1.getName() : Player2.getName()}`);
-    }
+    )
+      displayController.displayWinner(currentPlayer);
+
     // Diagonal
     if (
       gameArray[2][0] !== 0 &&
       gameArray[2][0] === gameArray[1][1] &&
       gameArray[1][1] === gameArray[0][2]
-    ) {
-      console.log(`Winner is ${Player1.active === true ? Player1.getName() : Player2.getName()}`);
-    }
+    )
+      displayController.displayWinner(currentPlayer);
+    // Check for a draw. Original array contains all 0, if there are no winners and all the array
+    // is filled with X and O, that would mean there are no 0's(empty spots) left
+    if (!gameArray.some((row) => row.includes(0))) displayController.displayWinner("draw");
   };
 
-  return { updateArray, switchPlayers, checkWin };
+  return { checkWin };
 })();
 
-document.querySelector("button").addEventListener("click", () => {
-  displayController.displayBoard(gameBoard.gameArray);
+document.querySelector("#btn-start").addEventListener("click", () => {
+  displayController.displayBoard();
+  gameBoard.setPlayers();
+  gameBoard.createArray();
+  document.querySelector("#player-info-container").style.display = "none";
   document.querySelectorAll("[data-cell]").forEach((cell) =>
     cell.addEventListener("click", (e) => {
-      // console.log(e);
+      if (updateGame.gameEnd) return;
       displayController.displaySelection(e.target);
-      updateGame.updateArray(e.target, gameBoard);
-      updateGame.checkWin(gameBoard.gameArray);
+      updateGame.updateArray(e.target);
+      gameStatus.checkWin(gameBoard.getArray(), displayController);
       updateGame.switchPlayers();
     })
   );
+});
+
+document.querySelector("#replay").addEventListener("click", (e) => {
+  document.querySelector("#container").style.display = "none";
+  document.querySelector("#container").innerHTML = "";
+  document.querySelectorAll("input").forEach((input) => (input.value = ""));
+  document.querySelector("#player-info-container").style.display = "block";
+  gameBoard.gameArray = [];
+  gameBoard.playerArray = [];
+  updateGame.gameEnd = false;
+  e.target.style.display = "none";
 });
